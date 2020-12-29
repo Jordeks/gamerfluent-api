bb4442dc0dc8f76c91e5da63afaf0eb7ed3a2576
MMcClure11 <mcclme02@luther.edu>
2020-12-29 10:47:10 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u1 = User.create(name: "Meks M", username: "Meks", password: "1234")
+u2 = User.create(name: "Max M", username: "MadMax", password: "1234")
+
+b1 = u1.blogs.build(title: "Second Blog Post", content: "Testing a new blog post")
+b2 = u1.blogs.build(title: "Third Blog Post", content: "Testing another new blog post")
+b3 = u2.blogs.build(title: "Fourth Blog Post", content: "We love writing blogs!")
+
+b1.save
+b2.save
+b3.save
