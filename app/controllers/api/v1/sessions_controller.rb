17bc510e888597f8a3573daf480a5b6966f73924
MMcClure11 <mcclme02@luther.edu>
2021-01-21 10:29:17 -0600

    Add auto_login action and session_user method

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -10,5 +10,13 @@ class Api::V1::SessionsController < ApplicationController
       render json: {failure: "Login failed! Username or password invalid."}
     end
   end
+
+  def auto_login
+    if session_user
+      render json: session_user
+    else
+      render json: { errors: "No User Logged In"}
+    end
+  end
   
 end
