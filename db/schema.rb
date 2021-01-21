483751991a3921ce8c3e085b3803c18631b79cb8
MMcClure11 <mcclme02@luther.edu>
2021-01-21 11:32:17 -0600

    Add blurb and image to blogs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_29_163719) do
+ActiveRecord::Schema.define(version: 2021_01_21_172915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2020_12_29_163719) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
+    t.text "blurb"
     t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
